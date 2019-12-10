671602fdc2b523fddc84b28cbdf28222cd760a21
suzu nagano <sszz12121994@gmail.com>
2019-12-10 12:45:04 +0900

    [add/fix]各種リントとプラグインなどのパッケージ追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
   env: {
     browser: true,
     node: true
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  globals: {
+    browser: true,
+    Snap: true,
+    Typekit: true,
+    // PIXI: true
   },
   extends: [
     '@nuxtjs',
@@ -15,9 +22,22 @@ module.exports = {
     'plugin:nuxt/recommended'
   ],
   plugins: [
+    'vue',
     'prettier'
   ],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }
+    ]
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+import path from 'path'
+import StylelintPlugin from 'stylelint-webpack-plugin'
+
 export default {
   mode: 'universal',
   /*
@@ -5,6 +8,10 @@ export default {
    */
   head: {
     title: process.env.npm_package_name || '',
+    titleTemplate: '',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -12,9 +19,28 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
+      },
+      {
+        hid: 'msapplication-TileColor',
+        name: 'msapplication-TileColor',
+        content: '#bf4040'
+      },
+      {
+        hid: 'theme-color',
+        name: 'theme-color',
+        content: '#ffffff'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: '#bf4040' }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -23,17 +49,23 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/scss/style.scss'],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: '~/plugins/routerOption.js',
+      mode: 'client'
+    }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module'
   ],
   /*
    ** Nuxt.js modules
@@ -41,8 +73,25 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
+    'nuxt-user-agent',
+    'nuxt-webfontloader'
   ],
+  /*
+   ** globalのscss
+   */
+  styleResources: {
+    // scss: ['~/assets/scss/mixin.scss']
+  },
+  /*
+   ** webfontloader
+   */
+  webfontloader: {
+    google: {
+      families: ['Sawarabi+Gothic', 'Libre+Baskerville']
+    }
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
@@ -55,6 +104,46 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      // クライアントのバンドルの Webpack 設定のみを拡張する
+      if (ctx.isClient && ctx.isDev) {
+        // create sourcemaps
+        config.devtool = 'source-map'
+        // Run ESLint on save
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+        // Run styleLint
+        config.plugins.push(
+          new StylelintPlugin({
+            files: ['**/*.vue', '**/*.scss']
+          })
+        )
+      }
+      // import alias
+      config.resolve.alias.Sass = path.resolve(__dirname, './assets/scss/')
+      config.resolve.alias.Images = path.resolve(__dirname, './assets/img/')
+      config.resolve.alias['~'] = path.resolve(__dirname)
+      config.resolve.alias['@'] = path.resolve(__dirname)
+    },
+    // generate時にコンソールを削除する
+    terser: {
+      terserOptions: {
+        compress: { drop_console: true }
+      }
+    }
+  },
+  /*
+   ** generate
+   */
+  generate: {
+    routes(callback) {
+      const pontPosts = require('./assets/data/point.json')
+      const pointRoutes = pontPosts.map((post) => `/point/${post.id}`)
+      callback(null, [...pointRoutes])
+    }
   }
 }
